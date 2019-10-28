9aed31dd8ea8b34ce2518f8dddb01ddd5e251142
Quentin <quentin.ratajski@diiage.org>
2019-10-28 14:24:32 +0100

    Navigation from "Training Sessions Tabs" to each tab.

--- app/src/main/java/org/diiage/dtrqandroid/trainingSessions/TrainingSessionsTabsFragment.java
+++ app/src/main/java/org/diiage/dtrqandroid/trainingSessions/TrainingSessionsTabsFragment.java
@@ -5,18 +5,24 @@ import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.material.tabs.TabLayout;
+
 import org.diiage.dtrqandroid.R;
+import org.diiage.dtrqandroid.trainingSessions.ViewPager.TrainingSessionsTabsFragmentPagerAdapter;
 
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
  */
 public class TrainingSessionsTabsFragment extends Fragment {
+
+    private TrainingSessionsTabsFragmentPagerAdapter pagerAdapter;
 
     public TrainingSessionsTabsFragment() {
         // Required empty public constructor
@@ -31,7 +37,17 @@ public class TrainingSessionsTabsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_training_sessions_tabs, container, false);
+        View view = inflater.inflate(R.layout.fragment_training_sessions_tabs, container, false);
+
+        pagerAdapter = new TrainingSessionsTabsFragmentPagerAdapter(this.getChildFragmentManager(), this.getContext());
+        ViewPager viewPager = view.findViewById(R.id.training_sessions_tabs_view_pager);
+        viewPager.setAdapter(pagerAdapter);
+
+        TabLayout tabLayout = view.findViewById(R.id.training_sessions_tabs_tab_layout);
+        tabLayout.setupWithViewPager(viewPager);
+
+        // Inflate the layout for this fragment
+        return view;
     }
 
 }
