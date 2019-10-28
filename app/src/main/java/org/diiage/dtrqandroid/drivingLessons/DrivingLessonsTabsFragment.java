37110c7b33da3e689789dbb971b7b0d07c493ebc
Quentin <quentin.ratajski@diiage.org>
2019-10-28 14:05:20 +0100

    Navigation from "Driving Lesson Tabs" to each tab.

--- app/src/main/java/org/diiage/dtrqandroid/drivingLessons/DrivingLessonsTabsFragment.java
+++ app/src/main/java/org/diiage/dtrqandroid/drivingLessons/DrivingLessonsTabsFragment.java
@@ -1,22 +1,26 @@
 package org.diiage.dtrqandroid.drivingLessons;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.material.tabs.TabLayout;
+
 import org.diiage.dtrqandroid.R;
+import org.diiage.dtrqandroid.drivingLessons.ViewPager.DrivingLessonsTabsFragmentPagerAdapter;
 
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
  */
 public class DrivingLessonsTabsFragment extends Fragment {
+
+    private DrivingLessonsTabsFragmentPagerAdapter pagerAdapter;
 
     public DrivingLessonsTabsFragment() {
         // Required empty public constructor
@@ -30,8 +34,17 @@ public class DrivingLessonsTabsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_driving_lessons_tabs, container, false);
+
+        pagerAdapter = new DrivingLessonsTabsFragmentPagerAdapter(this.getChildFragmentManager(), this.getContext());
+        ViewPager viewPager = view.findViewById(R.id.driving_lessons_tabs_view_pager);
+        viewPager.setAdapter(pagerAdapter);
+
+        TabLayout tabLayout = view.findViewById(R.id.driving_lessons_tabs_tab_layout);
+        tabLayout.setupWithViewPager(viewPager);
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_driving_lessons_tabs, container, false);
+        return view;
     }
 
 }
